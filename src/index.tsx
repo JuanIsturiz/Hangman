fff9e8ebe0dd3cae728935173f89478ec3b10b6c
JuanIsturiz <juanandresisturiz@gmail.com>
2022-11-18 20:34:23 -0400

    fixed lives icon

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -8,9 +7,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
